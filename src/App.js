3d06fe156a3771822c535d97fd0803f07937adf0
Prakash Shaw <prakashshaw1577@gmail.com>
2022-09-12 09:59:11 +0530

    Homepage completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path='movie/:id' element={<h1>Movie Detail Show.</h1>} />
+          <Route path='movies/:type' element={<h1>Type of Movie.</h1>} />
+          <Route path='/*' element={<h1>Error Page</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/card/Card.js
@@ -0,0 +1,5 @@
+import React from "react";
+import {useEffect, useState} from "react";
+import Skeleton, {SkeletonTheme} from "react-loading-skeleton";
+import './Card.css';
+import { Link } from "react-router-dom";
