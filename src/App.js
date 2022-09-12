5f4222b26f5edbb6dcab3556cffa1ef9671dca22
Prakash Shaw <prakashshaw1577@gmail.com>
2022-09-12 10:26:43 +0530

    card design completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home'
+import MovieList from './components/movieLists/Movielist';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />}></Route>
           <Route path='movie/:id' element={<h1>Movie Detail Show.</h1>} />
-          <Route path='movies/:type' element={<h1>Type of Movie.</h1>} />
+          <Route path='movies/:type' element={<MovieList />} />
           <Route path='/*' element={<h1>Error Page</h1>} />
         </Routes>
       </Router>
